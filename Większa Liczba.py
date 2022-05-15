ac8d09d725eef100dfda69e71dbe3262cdf9e210
Patrick <TheUnderscan@gmail.com>
2022-05-15 15:48:25 +0200

    Update Większa Liczba.py

--- Większa Liczba.py
+++ Większa Liczba.py
@@ -1,6 +1,6 @@
 # Ten program oblicza największa liczbe z dwóch podanych.
-int = a = (input("Proszę podać pierwszą liczbę : " )) # Tutaj wpisuje pierwszą zmienną.
-int = b = (input("Proszę podać drugą liczbę: " )) # Tutaj wpisuje drugą zmienną.
+ a = int(input("Proszę podać pierwszą liczbę : " )) # Tutaj wpisuje pierwszą zmienną.
+ b = int(input("Proszę podać drugą liczbę: " )) # Tutaj wpisuje drugą zmienną.
 if a>b: # Jeśli a > b, to:
     print("Większa liczba jest " + a + ", czyli pierwsza liczba którą wpisałeś.")
 else: # A jak inaczej to:
