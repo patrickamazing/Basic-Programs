0c685f2f39503a5e5a7e9122325facdf63a3c635
Patrick <TheUnderscan@gmail.com>
2022-05-21 15:04:52 +0200

    Updating WeightConverter.
    Added ounces and made it more readable.
    Soon making restart repeatable.

--- WeightConverter.py
+++ WeightConverter.py
@@ -1,26 +1,30 @@
-# This program will convert lb's to kg's or in reverse
+# This program will convert different weights.
 lbFactor = 2.204623
-roundingNumber = int(input("Input the number you will be rounding to: "))
+ozFactor = 35.27396195
 def main():
-    modeSelection = int(input("Please select your mode: \n1 = kg to lb.\n2 = lb to kg. "))
-    if modeSelection != 1 and modeSelection != 2:
+    modeSelection = int(input("Please select your mode: \n1 = kg to lb.\n2 = lb to kg.\n3 = kg to oz.\n4 = oz to kg. "))
+    if modeSelection != 1 and modeSelection != 2 and modeSelection != 3 and modeSelection != 4:
         print("Error: Incorrect mode.")
         main()
     else:
         inputWeight = int(input("Enter weight you will be converting: "))
         if modeSelection == 1:
-          print(inputWeight,"kg's converted to lb's is:",round(inputWeight*lbFactor, roundingNumber))
-        else:
-            print(inputWeight,"lb's converted to kg's is:",round(inputWeight/lbFactor, roundingNumber))
-    def restarting():
-        restart = input("Do you want to convert again? (y/n) ")
-        if restart != 'y' and restart != 'n':
-            print("Error.")
-            restarting()
-        elif restart == 'y':
-            main()
-        elif restart == 'n':
-            input("Have a good day!\nPress enter to exit.")
-            quit()
-    restarting()
+          print(inputWeight,"kg's converted to lb's is:",round(inputWeight*lbFactor, 3))
+        elif modeSelection == 2:
+            print(inputWeight,"lb's converted to kg's is:",round(inputWeight/lbFactor, 3))
+        elif modeSelection == 3:
+            print(inputWeight,"kg's converted to oz's is:",round(inputWeight*ozFactor, 3))
+        elif modeSelection == 4:
+            print(inputWeight,"oz's converted to kg's is:",round(inputWeight/ozFactor, 3))
 main()
+def restarting():
+    restart = input("Do you want to convert again? (y/n) ")
+    if restart != 'y' and restart != 'n':
+        print("Error.")
+        restarting()
+    elif restart == 'y':
+        main()
+    elif restart == 'n':
+        input("Have a good day!\nPress enter to exit.")
+        quit()
+restarting()
