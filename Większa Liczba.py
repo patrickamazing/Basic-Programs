fbc7c15b4b55e151d33c5897a742b689dbe0e4e9
Patrick <TheUnderscan@gmail.com>
2022-05-21 15:16:49 +0200

    Update Większa Liczba.py
    A bit more complicated ;)
    Although better and added restart function.
    Soon to be fixed.

--- Większa Liczba.py
+++ Większa Liczba.py
@@ -1,9 +1,21 @@
-# Ten program oblicza największa liczbe z dwóch podanych.
- a = int(input("Proszę podać pierwszą liczbę : " )) # Tutaj wpisuje pierwszą zmienną.
- b = int(input("Proszę podać drugą liczbę: " )) # Tutaj wpisuje drugą zmienną.
-if a>b: # Jeśli a > b, to:
-    print("Większa liczba jest " + a + ", czyli pierwsza liczba którą wpisałeś.")
-else: # A jak inaczej to:
-    print("Większa liczba jest " + b + ", czyli druga liczba którą wpisałeś.")
-
-exit() # Prośba programu aby zakończyć siebię.
+# This program will give you the biggest number out of the ones inputted.
+def main():
+    firstInt = int(input("Input the first number: " ))
+    secondInt = int(input("Input the second number: " ))
+    if firstInt > secondInt:
+        print("The bigger number is:" + firstInt + ",  the first number you inputted.")
+    elif firstInt < secondInt:
+        print("The bigger number is: " + secondInt + ", the second number you inputted.")
+    def restarting():
+        restart = input("Do you want to run this program again? (y/n) ")
+        if restart != 'y' and restart != 'n':
+            print("Error.")
+            restarting()
+        elif restart == 'y':
+            main()
+        elif restart == 'n':
+            print("Have a good day! :)")
+            input("Press enter to exit. ")
+    restarting()    
+main()
+quit()
